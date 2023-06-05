c733ae9de45b2b010d12eec1fef00698d2af7581
Divya Rajput <divya.rajput8220@gmail.com>
2023-06-04 23:49:58 -0700

    Project-4

---
+++ BOOK_STORE/book_store/outlet/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Book,Author,Country,Address
+
+# Register your models here.
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields ={"slug": ("title",)}
+    list_display = ("title","author",)
+    list_filter = ("author","rating",)
+
+admin.site.register(Book,BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
---
+++ BOOK_STORE/book_store/outlet/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Book
+from django.http import Http404
+from django.db.models import Avg,Max,Min
+
+# Create your views here.
+def index(request):
+    books = Book.objects.all().order_by("title")
+    num_books = books.count()
+    avg_rating = books.aggregate(Avg("rating"))
+    return render(request,"outlet/index.html",{
+        "books": books,
+        "total_number_of_books": num_books,
+        "average_rating": avg_rating,
+    })
+
+def book_detail(request,slug):
+    # try: 
+    #     book =  Book.objects.get(pk=id)
+    # except:
+    #     raise Http404()
+    book = get_object_or_404(Book,slug=slug)
+    return render(request,"outlet/book_detail.html",{
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestselling": book.is_bestselling,
+    })
